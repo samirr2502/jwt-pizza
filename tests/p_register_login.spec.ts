e6cbedf3e3055ae86be11577851b9d91b60ac12b
samirr2502 <samirrodriguez2502@gmail.com>
2025-10-14 12:14:43 -0600

    update(user.spec.ts) made sure it passed without server running (just using mocks)

--- tests/p_register_login.spec.ts
+++ tests/p_register_login.spec.ts
@@ -1,66 +1,60 @@
 import { test, expect } from 'playwright-test-coverage';
 
 test('login admin', async ({ page }) => {
-await page.route('*/**/api/auth', async (route) => {
-  const loginReq = { email: 'd@jwt.com', password: 'a' };
-  const loginRes = {
-    user: {
-      id: 3,
-      name: 'Kai Chen',
-      email: 'd@jwt.com',
-      roles: [{ role: 'admin' }],
-    },
-    token: 'abcdef',
-  };
-  expect(route.request().method()).toBe('PUT');
-  expect(route.request().postDataJSON()).toMatchObject(loginReq);
-  await route.fulfill({ json: loginRes });
-});
+  await page.route('*/**/api/auth', async (route) => {
+    const loginReq = { email: 'd@jwt.com', password: 'a' };
+    const loginRes = {
+      user: {
+        id: 3,
+        name: 'Kai Chen',
+        email: 'd@jwt.com',
+        roles: [{ role: 'admin' }],
+      },
+      token: 'abcdef',
+    };
+    expect(route.request().method()).toBe('PUT');
+    expect(route.request().postDataJSON()).toMatchObject(loginReq);
+    await route.fulfill({ json: loginRes });
+  });
 
 
-    await page.goto('http://localhost:5173/');
-    await page.getByRole('link', { name: 'Login' }).click();
-    await page.getByRole('textbox', { name: 'Email address' }).fill('d@jwt.com');
-    await page.getByRole('textbox', { name: 'Password' }).fill('a');
-    await page.locator('div').filter({ hasText: /^Login$/ }).click();
-    await expect(page.getByRole('link', { name: 'Order' })).toBeVisible();
+  await page.goto('http://localhost:5173/');
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('d@jwt.com');
+  await page.getByRole('textbox', { name: 'Password' }).fill('a');
+  await page.locator('div').filter({ hasText: /^Login$/ }).click();
+  await expect(page.getByRole('link', { name: 'Order' })).toBeVisible();
 
-    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
 
-    // await page.getByRole('link', { name: 'Order' }).click();
-
-
-    // await page.locator('.flex.flex-col.group').first().click();
-    // await expect(page.getByText('Selected pizzas:')).toBeVisible();
-    // await page.getByRole('button', { name: 'Checkout' }).click();
 });
 
 test('register diner', async ({ page }) => {
-await page.route('*/**/api/auth', async (route) => {
-  const registerReq = {name:"test1", email: 't@jwt.com', password: 't' };
-  const registerRes = {
-    user: {
-      id: 3,
-      name: 'test',
-      email: 't@jwt.com',
-      roles: [{ role: 'diner' }],
-    },
-    token: 'abcdef',
-  };
-  expect(route.request().method()).toBe('POST');
-  expect(route.request().postDataJSON()).toMatchObject(registerReq);
-  await route.fulfill({ json: registerRes });
-});
+  await page.route('*/**/api/auth', async (route) => {
+    const registerReq = { name: "test1", email: 't@jwt.com', password: 't' };
+    const registerRes = {
+      user: {
+        id: 3,
+        name: 'test',
+        email: 't@jwt.com',
+        roles: [{ role: 'diner' }],
+      },
+      token: 'abcdef',
+    };
+    expect(route.request().method()).toBe('POST');
+    expect(route.request().postDataJSON()).toMatchObject(registerReq);
+    await route.fulfill({ json: registerRes });
+  });
 
 
-    await page.goto('http://localhost:5173/');
-    
-    await page.getByRole('link', { name: 'Register' }).click();
-    await page.getByRole('textbox', { name: 'Full name' }).fill('test1');
-    await page.getByRole('textbox', { name: 'Email address' }).fill('t@jwt.com');
-    await page.getByRole('textbox', { name: 'Password' }).fill('t');
-    await page.getByRole('button', { name: 'Register' }).click();
-    await expect(page.getByRole('link', { name: 'Order' })).toBeVisible();
+  await page.goto('http://localhost:5173/');
+
+  await page.getByRole('link', { name: 'Register' }).click();
+  await page.getByRole('textbox', { name: 'Full name' }).fill('test1');
+  await page.getByRole('textbox', { name: 'Email address' }).fill('t@jwt.com');
+  await page.getByRole('textbox', { name: 'Password' }).fill('t');
+  await page.getByRole('button', { name: 'Register' }).click();
+  await expect(page.getByRole('link', { name: 'Order' })).toBeVisible();
 
 
 });
--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,39 +1,94 @@
 import { test, expect } from 'playwright-test-coverage';
 
-test('updateUser', async ({ page }) => {
-    const email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
+test('update user', async ({ page }) => {
+
+    await page.route('*/**/api/auth', async (route) => {
+        const registerReq = { name: "pizza diner", email: 't@jwt.com', password: 't' };
+        const registerRes = {
+            user: {
+                id: 3,
+                name: 'pizza diner',
+                email: 't@jwt.com',
+                roles: [{ role: 'diner' }],
+            },
+            token: 'abcdef',
+        };
+        expect(route.request().method()).toBe('POST');
+        expect(route.request().postDataJSON()).toMatchObject(registerReq);
+        await route.fulfill({ json: registerRes });
+    });
+
+
+
+    const email = `t@jwt.com`;
     await page.goto('/');
     await page.getByRole('link', { name: 'Register' }).click();
     await page.getByRole('textbox', { name: 'Full name' }).fill('pizza diner');
     await page.getByRole('textbox', { name: 'Email address' }).fill(email);
-    await page.getByRole('textbox', { name: 'Password' }).fill('diner');
+    await page.getByRole('textbox', { name: 'Password' }).fill('t');
     await page.getByRole('button', { name: 'Register' }).click();
 
     await page.getByRole('link', { name: 'pd' }).click();
 
     await expect(page.getByRole('main')).toContainText('pizza diner');
-    await page.getByRole('button', { name: 'Edit' }).click();
-    await expect(page.locator('h3')).toContainText('Edit user');
-    await page.getByRole('button', { name: 'Update' }).click();
-
-    await page.waitForSelector('[role="dialog"].hidden', { state: 'attached' });
-
-    await expect(page.getByRole('main')).toContainText('pizza diner');
 
     //New input test
+
+    await page.route('*/**/api/user/3', async (route) => {
+        const updateReq = { name: "pizza dinerx", email: 't@jwt.com' };
+        const updateRes = {
+            user: {
+                id: 3,
+                name: 'pizza dinerx',
+                email: 't@jwt.com',
+                roles: [{ role: 'diner' }],
+            },
+            token: 'abcdef',
+        };
+        expect(route.request().method()).toBe('PUT');
+        expect(route.request().postDataJSON()).toMatchObject(updateReq);
+        await route.fulfill({ json: updateRes });
+    });
     await page.getByRole('button', { name: 'Edit' }).click();
+
     await expect(page.locator('h3')).toContainText('Edit user');
+
     await page.getByRole('textbox').first().fill('pizza dinerx');
     await page.getByRole('button', { name: 'Update' }).click();
 
     await page.waitForSelector('[role="dialog"].hidden', { state: 'attached' });
 
     await expect(page.getByRole('main')).toContainText('pizza dinerx');
+
+    await page.route('*/**/api/auth', async (route) => {
+        const logoutRes = {}
+        expect(route.request().method()).toBe('DELETE');
+        // expect(route.request().postDataJSON()).toMatchObject(logoutReq);
+        await route.fulfill({ json: logoutRes });
+    });
+
     await page.getByRole('link', { name: 'Logout' }).click();
+
+    await page.route('*/**/api/auth', async (route) => {
+    const loginReq = { email: 't@jwt.com', password: 't' };
+    const loginRes = {
+      user: {
+        id: 3,
+        name: 'pizza dinerx',
+        email: 't@jwt.com',
+        roles: [{ role: 'diner' }],
+      },
+      token: 'abcdef',
+    };
+    expect(route.request().method()).toBe('PUT');
+    expect(route.request().postDataJSON()).toMatchObject(loginReq);
+    await route.fulfill({ json: loginRes });
+  });
     await page.getByRole('link', { name: 'Login' }).click();
 
+
     await page.getByRole('textbox', { name: 'Email address' }).fill(email);
-    await page.getByRole('textbox', { name: 'Password' }).fill('diner');
+    await page.getByRole('textbox', { name: 'Password' }).fill('t');
     await page.getByRole('button', { name: 'Login' }).click();
 
     await page.getByRole('link', { name: 'pd' }).click();
